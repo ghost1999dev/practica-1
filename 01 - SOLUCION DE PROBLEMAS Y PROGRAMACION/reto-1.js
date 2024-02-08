e1d0b06de8140176190dd5e7b2d16426ebb744dd
fernandoDev <blancogonzalezfernandoalexis@gmail.com>
2024-02-07 18:26:27 -0600

    feat:Se subio la actualizacion de codigo

---
+++ 01 - SOLUCION DE PROBLEMAS Y PROGRAMACION/reto-1.js
@@ -0,0 +1,48 @@
+
+//COMENTARIOS
+//PROGRAMAR LOS OPERADORES ARITMETICOS
+//suma
+let suma = 1+1
+//resta
+let resta = 1-1
+//multiplicacion
+let multiplicacion = 2*2
+//division
+let division = 2/2;
+//resto
+let modulo = 2%2
+//exponente
+let exponente = 2 ** 3
+
+//console.log('SUMA: ' + suma + ' RESTA ' + resta + ' MULTIPLICACION ' +multiplicacion);
+
+//operadores de comparacion
+
+//valores globales
+let x = 4
+let y = 5
+let z = 1
+
+//comparacion de igualdad
+//console.log(y == '5');
+//comparacion de igualdad si es diferente
+//console.log(y != 3);
+
+//console.log(y === 5);
+
+//console.log( y !== 3);
+
+//operadores de desicion 
+
+/*if (y >= 5) {
+
+    console.log("Y ES MAYOR QUE CINCO");
+    
+}else{
+    console.log("Y ES MENOR QUE CINCO");
+}*/
+
+let edad = 15
+
+//ternario
+edad > 18 ? console.log("Es mayor de edad"):console.log("Es menor de edad");
