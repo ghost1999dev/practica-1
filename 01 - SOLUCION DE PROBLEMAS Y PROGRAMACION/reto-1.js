59f51ba57374d8874e9410602bf2effb744b4eb6
fernandoDev <blancogonzalezfernandoalexis@gmail.com>
2024-02-14 10:19:10 -0600

    feat:Se modifico el archivo de la sesion2

--- 01 - SOLUCION DE PROBLEMAS Y PROGRAMACION/reto-1.js
+++ 01 - SOLUCION DE PROBLEMAS Y PROGRAMACION/reto-1.js
@@ -48,77 +48,150 @@ let z = 1
 //ternario
 //edad > 18 ? console.log("Es mayor de edad"):console.log("Es menor de edad");
 
+/*const nota=10;
 
-const objectBooks=[
-    {
-      "name": "Dune",
-      "author": "Frank Herbert",
-      "year": "1965"
-    },
-    {
-      "name": "1984",
-      "author": "George Orwell",
-      "year": "1949"
-    },
-    {
-      "name": "Foundation",
-      "author": "Isaac Asimov",
-      "year": "1951"
-    },
-    {
-      "name": "The Hitchhiker's Guide to the Galaxy",
-      "author": "Douglas Adams",
-      "year": "1979"
-    },
-    {
-      "name": "Neuromancer",
-      "author": "William Gibson",
-      "year": "1984"
-    },
-    {
-      "name": "Ender's Game",
-      "author": "Orson Scott Card",
-      "year": "1985"
-    },
-    {
-      "name": "Brave New World",
-      "author": "Aldous Huxley",
-      "year": "1932"
-    },
-    {
-      "name": "Fahrenheit 451",
-      "author": "Ray Bradbury",
-      "year": "1953"
-    },
-    {
-      "name": "The War of the Worlds",
-      "author": "H.G. Wells",
-      "year": "1898"
-    },
-    {
-      "name": "Snow Crash",
-      "author": "Neal Stephenson",
-      "year": "1992"
-    }
-  ]
-
-/*for(const element of objectBooks){
-    console.log(element.name);
+if (nota <= 8) {
+  console.log("No paso la materia");
+  
+}else{
+  console.log("Si paso la materia");
 }*/
 
-let day = 'Monday';
-let element='';
-switch (day) {
-    case 'Sunday':
-        element = 'Dia domingo'
-        
-        break;
-    case 'Monday':
-        element = 'Dia lunes'
 
-    default:
-        break;
-    
+/**
+ * *ESTRUCTURA FOR
+ */
+/*let iteracion =0
+
+for (iteracion; iteracion < 50; iteracion++) {
+  console.log(iteracion);
+}*/
+
+/**
+ * *ESTRUCTURA FOR OF
+ */
+
+//ARREGLO
+
+/*const iterables=[
+  'trenes',
+  'automoviles',
+  'camiones'
+]
+
+for(let value of iterables){
+  console.log(value);
+}*/
+
+/**
+ * *ESTRUCTURA FOR IN
+ */
+
+/*const frutas={
+  nombre:"manzana",
+  precio:"1.00",
+  peso:"2.5kg",
+  cantidad:''
 }
 
-console.log(element);
+for(const propiedad in frutas){
+  console.log(frutas[propiedad]);
+}*/
+
+/**
+ * * ESTRUCTURA DE CONTROL SWITCH 
+ */
+
+/*const instrumento = 'flauta'
+switch (instrumento) {
+  case 'Guitarra':
+    console.log('Has seleccionado guitarra');
+    break;
+  case 'Piano':
+    console.log('Has seleccionado guitarra');
+    break;
+  case 'flauta':
+    console.log('Has seleccionado flauta');
+  default:
+    break;
+}*/
+
+
+/*let jsonData=[
+  {
+    "name": "Dune",
+    "author": "Frank Herbert",
+    "year": 1965
+  },
+  {
+    "name": "Ender's Game",
+    "author": "Orson Scott Card",
+    "year": 1985
+  },
+  {
+    "name": "1984",
+    "author": "George Orwell",
+    "year": 1949
+  },
+  {
+    "name": "The Hitchhiker's Guide to the Galaxy",
+    "author": "Douglas Adams",
+    "year": 1979
+  },
+  {
+    "name": "Brave New World",
+    "author": "Aldous Huxley",
+    "year": 1932
+  },
+  {
+    "name": "Foundation",
+    "author": "Isaac Asimov",
+    "year": 1951
+  },
+  {
+    "name": "Neuromancer",
+    "author": "William Gibson",
+    "year": 1984
+  },
+  {
+    "name": "Snow Crash",
+    "author": "Neal Stephenson",
+    "year": 1992
+  },
+  {
+    "name": "The Martian",
+    "author": "Andy Weir",
+    "year": 2011
+  },
+  {
+    "name": "Ready Player One",
+    "author": "Ernest Cline",
+    "year": 2011
+  }
+]
+
+for(let value of jsonData){
+  console.log(value.author);
+}*/
+
+
+/**
+ * * HACER UN PROGRAMA  QUE IMPRIMA POR CONSOLA
+ * * TODOS LOS NUMEROS COMPRENDIDOS DE 10 HASTA 55
+ * * (INCLUIDOS) PARES, IMPARES Y QUE NO APAREZCA EL 16
+ * * QUE NO SEAN MULTIPLOS DE 3
+ */
+
+for (let index = 10; index < 56; index++) {
+  
+  if (index % 2 ==0 && index % 3 !==0 && index !==16) {
+    console.log(index);
+    
+  }
+  
+}
+
+
+
+
+
--- 02 - SOLUCION DE PROBLEMAS Y PROGRAMACION (ESTRUCTURAS DE CONTROL) PARTE2/ejercicioTarea.md
+++ 02 - SOLUCION DE PROBLEMAS Y PROGRAMACION (ESTRUCTURAS DE CONTROL) PARTE2/ejercicioTarea.md
@@ -7,16 +7,13 @@
 *
 * Crea un programa que calcule el daño de un ataque
 * durante una batalla de pokemon
-* - La formula sera la siguiente: daño = 50
-*  (ataque / defensa) * efectividad
+* - La formula sera la siguiente: daño = 50 (ataque / defensa) * efectividad
 * - Efectivad: x2 (super efectivo), x1 (neutral),
-* x0.5 (no es muy efectivo) se requiere buscar su * efectividad
+*   x0.5 (no es muy efectivo) se requiere buscar su * efectividad
 * - El programa recibe los siguientes parametros
 *   - Tipo de Pokemon atacante
 *   - Tipo de Pokemon defensor.
 *   - Ataque: Entre 1 y 100.
 *   - Defensa: Entre 1 y 100.
 */
-
-
 ```
